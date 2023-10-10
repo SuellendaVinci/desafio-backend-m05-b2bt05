2de0760d89908229f096fb8ec176523fb082bf96
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-10-10 19:38:03 -0300

    feat: criar o controlador para atualizar o usuário

---
+++ src/controladores/usuarios/atualizar.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const knex = require("../../conexao");
+
+const atualizar = async (req, res) => {
+  const usuario = req.usuario;
+  const { nome, email, senha } = req.body;
+
+  if (!nome || !email || !senha) {
+    return res
+      .status(400)
+      .json({ mensagem: "Todos os campos são obrigatórios" });
+  }
+
+  try {
+    const usuarioExiste = await knex("usuarios").where({ email }).first();
+
+    if (usuarioExiste && usuarioExiste.id !== usuario.id) {
+      return res
+        .status(400)
+        .json({ mensagem: "Já existe um usuário com esse e-mail" });
+    }
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    const usuarioAtualizado = await knex("usuarios")
+      .update({ nome, email, senha: senhaCriptografada })
+      .where({ id: usuario.id });
+
+    return res.status(204).json(usuarioAtualizado);
+  } catch (erro) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
+
+module.exports = { atualizar };
