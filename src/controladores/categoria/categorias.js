7c84f3625cad6ffe4ed442e8f9eebc7a9541f920
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2023-10-10 06:14:41 -0300

    feat: criação da rota cadastrar usuário

--- src/controladores/categoria/categorias.js
+++ src/controladores/categoria/categorias.js
@@ -1,4 +1,4 @@
-const knex = require('../conexao');
+const knex = require('../../conexao');
 
 const listarCategorias = async (req, res) => {
 
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const categorias = require('./controladores/categorias');
+const categorias = require('./controladores/categoria/categorias');
+const cadastrar = require('./controladores/usuarios/cadastrar');
 
 const rotas = express();
 
 rotas.get('/categoria', categorias.listarCategorias);
+rotas.post('/usuario', cadastrar)
 
 module.exports = rotas;
