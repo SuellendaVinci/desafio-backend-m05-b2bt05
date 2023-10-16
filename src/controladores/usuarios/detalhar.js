13017c276993e1307aaa41dcee0996d004129099
Lorena Paixão <lorenamagalhaes4127@gmail.com>
2023-10-16 01:05:00 -0300

    fix/corrigindoCodigo

--- src/controladores/usuarios/detalhar.js
+++ src/controladores/usuarios/detalhar.js
@@ -1,23 +1,7 @@
-const knex = require("../../conexao");
 
+	const detalhar = async (req, res) => {
 
-const detalhar = async (req, res) => {
-    try {
-      const detalharUsuario = req.detalhar.usuario;
-  
-      const usuario = await knex('usuarios')
-        .select('id', 'nome', 'email')
-        .where({ id: detalharUsuario })
-        .first();
-  
-      if (!usuario) {
-        return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-      }
-  
-      return res.json(usuario);
-    } catch (error) {
-      return res.status(500).json({ mensagem: 'Erro interno do servidor' });
-    }
-  }
+    		return res.status(200).json(req.usuario);
+	}
 
-  module.exports = detalhar;
+	module.exports = detalhar;
