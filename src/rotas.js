c2341830ac7695e21d5842c6fdac40db4183b3e8
Renata Luz <renataluz@users.noreply.github.com>
2023-10-22 04:33:44 -0300

    refatoração e padronização de usuários

--- src/rotas.js
+++ src/rotas.js
@@ -19,8 +19,6 @@ const schemaCliente = require("./validacoes/schemaCliente");
 const postCliente = require("./controladores/cliente/cadastrar");
 const putCliente = require("./controladores/cliente/atualizar");
 const listaProdutos = require("./controladores/produtos/listar");
-const excluiProduto = require("./controladores/produtos/excluir");
-const listarClientes = require("./controladores/cliente/listar");
 const rotas = express();
 
 rotas.get("/categoria", getCategorias);
@@ -35,10 +33,8 @@ rotas.put('/usuario', validarCorpoRequisicao(schemaUsuario), atualizar);
 rotas.post('/produto', validarCorpoRequisicao(schemaProduto), cadastraProduto)
 rotas.get('/produto', listaProdutos)
 rotas.get('/produto/:id', listaProdutos)
-rotas.delete('/produto/:id', excluiProduto)
+
 rotas.post('/cliente', validarCorpoRequisicao(schemaCliente), postCliente);
 rotas.put('/cliente/:id', validarCorpoRequisicao(schemaCliente), putCliente);
-rotas.get(['/cliente', '/cliente/:id'], listarClientes);
-
 
 module.exports = rotas;
