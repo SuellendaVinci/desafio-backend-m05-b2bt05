7c1b03db076640fb4573a7bf66be5e16b79328da
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2023-10-24 03:51:54 -0300

    database: criação das tabelas pedidos e pedido-produtos

--- src/servicos/bancoDeDados/dump.sql
+++ src/servicos/bancoDeDados/dump.sql
@@ -6,6 +6,10 @@ DROP TABLE IF EXISTS produtos;
 
 DROP TABLE IF EXISTS clientes;
 
+DROP TABLE IF EXISTS pedidos;
+
+DROP TABLE IF EXISTS pedido_produtos;
+
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -23,18 +27,34 @@ CREATE TABLE produtos (
     descricao VARCHAR(255) NOT NULL,
     quantidade_estoque INTEGER NOT NULL,
     valor INTEGER NOT NULL,
-    categoria_id INTEGER NOT NULL REFERENCES categorias(id)
+    categoria_id INTEGER NOT NULL REFERENCES categorias(id),
+    produto_imagem VARCHAR(255)
 );
 
 CREATE TABLE clientes (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL unique,
-    cpf VARCHAR(11) NOT NULL unique,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     cep VARCHAR(8),
     rua VARCHAR(255),
     numero VARCHAR(10),
     bairro VARCHAR(50),
     cidade VARCHAR(50),
     estado CHAR(02)
+);
+
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    cliente_id INTEGER NOT NULL REFERENCES clientes(id),
+    observacao VARCHAR(255) NOT NULL,
+    valor_total INTEGER NOT NULL
+);
+
+CREATE TABLE pedido_produtos (
+    id SERIAL PRIMARY KEY,
+    pedido_id INTEGER NOT NULL REFERENCES pedidos(id),
+    produto_id INTEGER NOT NULL REFERENCES produtos(id),
+    quantidade_produto INTEGER NOT NULL,
+    valor_produto INTEGER NOT NULL
 );
